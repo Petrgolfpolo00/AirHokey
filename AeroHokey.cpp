475b98945fd783fee64885d8ae46240dde727413
Petrgolfpolo00 <petrgolfpolo@mail.ru>
2018-01-28 10:54:46 +0300

    Here was added struct Player
    At this commit we got away from a planty pf undependent numeric variable. Instead of this, we used structers. These chenges shuld help tosqueeze code and make it more clear.

--- AeroHokey.cpp
+++ AeroHokey.cpp
@@ -1,5 +1,10 @@
     #include "Txlib.h"
 
+struct Player
+    {
+    double x, y, vx, vy;
+    };
+
 void GameProcess ();
 void Physics (double* x, double* y, double* vx, double* vy, int dt,int isPlayer);
 void Control (double* vxPlayer1, double* vyPlayer1, double* vxPlayer2, double* vyPlayer2);
@@ -16,9 +21,9 @@ int main ()
 
 void GameProcess ()
     {
-    double xWasher = 500, yWasher = 300, vxWasher = 50, vyWasher = 50;
-    double xPlayer1 = 50, yPlayer1 = 300, vxPlayer1 = 0, vyPlayer1 = 15;
-    double xPlayer2 = 950, yPlayer2 = 300, vxPlayer2 = 0, vyPlayer2 = 15;
+    Player washer  = {500, 300, 50, 50};
+    Player Player1 = {50, 300, 0, 15};
+    Player Player2 = {950, 300, 0, 15};
     int dt = 1;
 
     while ( ! GetAsyncKeyState (VK_RETURN))
@@ -34,29 +39,29 @@ void GameProcess ()
 
         txSetFillColor (TX_RED);
         txSetColor (TX_WHITE);
-        txCircle (xWasher, yWasher, 30);
+        txCircle (washer.x, washer.y, 30);
 
         txSetFillColor (TX_BLUE);
         txSetColor (TX_WHITE);
-        txCircle (xPlayer1, yPlayer1, 40);
+        txCircle (Player1.x, Player1.y, 40);
         txSelectFont ("Arial", 30);
-        txDrawText (xPlayer1 - 15, yPlayer1 - 15, xPlayer1 + 15, yPlayer1 + 15, "1");
+        txDrawText (Player1.x - 15, Player1.y - 15, Player1.x + 15, Player1.y + 15, "1");
 
 
         txSetFillColor (TX_BLUE);
         txSetColor (TX_WHITE);
-        txCircle (xPlayer2, yPlayer2, 40);
-        txDrawText (xPlayer2 - 15, yPlayer2 - 15, xPlayer2 + 15, yPlayer2 + 15, "2");
+        txCircle (Player2.x, Player2.y, 40);
+        txDrawText (Player2.x - 15, Player2.y - 15, Player2.x + 15, Player2.y + 15, "2");
 
 
-        Physics (&xWasher,  &yWasher,  &vxWasher,  &vyWasher,  dt, false);
-        Physics (&xPlayer1, &yPlayer1, &vxPlayer1, &vyPlayer1, dt, true);
-        Physics (&xPlayer2, &yPlayer2, &vxPlayer2, &vyPlayer2, dt, true);
+        Physics (&washer.x,  &washer.y,  &washer.vx,  &washer.vy,  dt, false);
+        Physics (&Player1.x, &Player1.y, &Player1.vx, &Player1.vy, dt, true);
+        Physics (&Player2.x, &Player2.y, &Player2.vx, &Player2.vy, dt, true);
 
-        Control (&vxPlayer1, &vyPlayer1, &vxPlayer2, &vyPlayer2);
+        Control (&Player1.vx, &Player1.vy, &Player2.vx, &Player2.vy);
 
-        Contact (xPlayer1, yPlayer1, vxPlayer1, vyPlayer1, &xWasher, &yWasher, &vxWasher, &vyWasher);
-        Contact (xPlayer2, yPlayer2, vxPlayer2, vyPlayer2, &xWasher, &yWasher, &vxWasher, &vyWasher);
+        Contact (Player1.x, Player1.y, Player1.vx, Player1.vy, &washer.x, &washer.y, &washer.vx, &washer.vy);
+        Contact (Player2.x, Player2.y, Player2.vx, Player2.vy, &washer.x, &washer.y, &washer.vx, &washer.vy);
 
         txSleep (100);
         }
